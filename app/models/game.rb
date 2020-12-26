b6112ec8cfd5fd3cae18bdfdaeaf7351e5deefce
Frank Dominguez <elvisf.dominguez@gmail.com>
2020-12-26 15:21:09 -0500

    Created basic model associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+  has_many :involved_companies, dependent: :destroy
+  has_many :companies, through: :involved_companies
+  has_and_belongs_to_many :genres
+  has_and_belongs_to_many :platforms
 end
