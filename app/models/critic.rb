6c4b35297a1b1f4646188ec208751e4cf50dcd3c
Frank Dominguez <elvisf.dominguez@gmail.com>
2020-12-27 19:50:27 -0500

    Implemented critics_count with counter cache in user model to be able to retrieve how many critics has a user

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,23 +1,25 @@
 class Critic < ApplicationRecord
-  belongs_to :user
+  # association belongs to, and count how many critics has a user.
+  belongs_to :user, counter_cache: true
+  # belongs_to :user, counter_cache: :critics_count
 
   belongs_to :criticable, polymorphic: true
 
-  # Update critics count
-  after_create :critics_count_plus_one
-  after_destroy :critics_count_less_one
-
-  private
-
-  def critics_count_plus_one
-    update_critics_count(1)
-  end
-
-  def critics_count_less_one
-    update_critics_count(-1)
-  end
-
-  def update_critics_count(num)
-    user.critics_count += num
-  end
+  #  # Update critics count
+  #  after_create :critics_count_plus_one
+  #  after_destroy :critics_count_less_one
+  #
+  #  private
+  #
+  #  def critics_count_plus_one
+  #    update_critics_count(1)
+  #  end
+  #
+  #  def critics_count_less_one
+  #    update_critics_count(-1)
+  #  end
+  #
+  #  def update_critics_count(num)
+  #    user.critics_count += num
+  #  end
 end
